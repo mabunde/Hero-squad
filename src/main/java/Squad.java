1948dbe694784564d59ee79027891c13eb1e442a
mabunde <solomonsitati39@gmail.com>
2021-11-01 19:28:54 +0300

    Add a constructor to the squad class

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,3 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Squad {
+    private String squadName;
+    private int maxSize;
+    private String squadCause;
+    private static List<Squad> instances = new ArrayList<Squad>();
+    private int mId;
+    private List<Hero> heroes;
+    private int currentSize;
+
+    public Squad (String squad_name, String squad_size, String squad_cause){
+        squadName = squad_name;
+        maxSize = Integer.parseInt(squad_size);
+        squadCause = squad_cause;
+        instances.add(this);
+        mId = instances.size();
+        heroes = new ArrayList<Hero>();
+        currentSize = 0;
+    }
 
 }
