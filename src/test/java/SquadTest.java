399964fed10335207831fe2ae39a95e7e490edc5
mabunde <solomonsitati39@gmail.com>
2021-11-01 20:47:16 +0300

    Formulate the first test

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -1,5 +1,6 @@
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -9,6 +10,11 @@ public class SquadTest {
     public void setUp() throws Exception {
     }
 
+    @Test
+    public void Squad_instantiatesCorrectly_true() {
+        Squad mySquad = new Squad("marvel", "6", "computer illiteracy");
+        assertEquals(true, mySquad instanceof Squad);
+    }
     @After
     public void tearDown() throws Exception {
     }
