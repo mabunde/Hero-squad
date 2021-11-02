2f46f5659fda8fe9e2764c5c8de70bdebf360071
mabunde <solomonsitati39@gmail.com>
2021-11-02 06:54:37 +0300

    Add squad template

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,12 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import static spark.Spark.*;
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
 
+
+    }
 }
